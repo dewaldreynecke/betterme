63bf26ecffe10e93dbbf83e29a93172770974096
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-07 11:21:11 +0200

    update seed for new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,60 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_152824) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_084740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_contacts_on_user_id"
-  end
-
   create_table "entries", force: :cascade do |t|
+    t.string "title"
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
-  create_table "entry_activities", force: :cascade do |t|
-    t.bigint "activity_id", null: false
-    t.bigint "entry_id", null: false
+  create_table "inspirations", force: :cascade do |t|
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["activity_id"], name: "index_entry_activities_on_activity_id"
-    t.index ["entry_id"], name: "index_entry_activities_on_entry_id"
-  end
-
-  create_table "entry_contacts", force: :cascade do |t|
-    t.bigint "entry_id", null: false
-    t.bigint "contact_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["contact_id"], name: "index_entry_contacts_on_contact_id"
-    t.index ["entry_id"], name: "index_entry_contacts_on_entry_id"
-  end
-
-  create_table "entry_prompts", force: :cascade do |t|
-    t.text "content"
-    t.bigint "prompt_id", null: false
-    t.bigint "entry_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["entry_id"], name: "index_entry_prompts_on_entry_id"
-    t.index ["prompt_id"], name: "index_entry_prompts_on_prompt_id"
   end
 
   create_table "moods", force: :cascade do |t|
@@ -72,12 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_152824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_moods_on_user_id"
-  end
-
-  create_table "prompts", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,13 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_152824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "contacts", "users"
   add_foreign_key "entries", "users"
-  add_foreign_key "entry_activities", "activities"
-  add_foreign_key "entry_activities", "entries"
-  add_foreign_key "entry_contacts", "contacts"
-  add_foreign_key "entry_contacts", "entries"
-  add_foreign_key "entry_prompts", "entries"
-  add_foreign_key "entry_prompts", "prompts"
   add_foreign_key "moods", "users"
 end
