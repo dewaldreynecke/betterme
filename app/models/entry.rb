34f9fb30144282839ed97a5634b95cc79bab1a92
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-07 10:36:32 +0200

    update model associations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,3 @@
 class Entry < ApplicationRecord
   belongs_to :user
-  has_many :contacts, through: :entry_contacts
-  has_many :prompts, through: :entry_prompts
-  has_many :activities, through: :entry_activities
-  enum type: %i[morning daily weekly monthly]
 end
-
