4ee95391753771179556b1e8d4b15dc220a1836a
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-07 20:34:56 +0200

    add stimulus actions

--- app/javascript/controllers/edit_title_controller.js
+++ app/javascript/controllers/edit_title_controller.js
@@ -5,6 +5,20 @@ export default class extends Controller {
   static targets = ["heading", "form", "edit", "check"]
 
   connect() {
-    
+    console.log("edit-movie controller connected")
+  }
+
+  displayForm() {
+    this.headingTarget.classList.add("d-none")
+    this.editTarget.classList.add("d-none")
+    this.formTarget.classList.remove("d-none")
+    this.checkTarget.classList.remove("d-none")
+  }
+
+  hideForm() {
+    this.headingTarget.classList.remove("d-none")
+    this.editTarget.classList.remove("d-none")
+    this.formTarget.classList.add("d-none")
+    this.checkTarget.classList.add("d-none")
   }
 }
