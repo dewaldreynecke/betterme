309ab3a8da56eabb5ca4a9bba83c1417a5645f5c
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-08 15:47:51 +0200

    migrate zenquote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_135511) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_134650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_135511) do
     t.string "theme"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "zenquotes", force: :cascade do |t|
+    t.text "quote"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
