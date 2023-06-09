ac0c6a71eebed79eeed01ffec14c7460c84c1dc1
Lesley Admin <masibi.mbl@gmail.com>
2023-06-09 12:44:37 +0200

    Map feature done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -22,12 +22,7 @@ class EntriesController < ApplicationController
   def show_by_date
     @date = Date.parse(params[:date])
     @entries_on_same_date = Entry.where(date: @date)
-    # @next_entry = Entry.where("date > ?", @date).order(date: :asc).first
-    # @previous_entry = Entry.where("date < ?", @date).order(date: :desc).first
-
-    @user = current_user
-    @entries = @user.entries
-    @markers = @entries.map do |entry|
+    @markers = @entries_on_same_date.geocoded.map do |entry|
     {
       lat: entry.latitude,
       lng: entry.longitude
@@ -42,4 +37,3 @@ class EntriesController < ApplicationController
   end
 end
 
-# to_date.beginning_of_day..params[:date].to_date.end_of_day
