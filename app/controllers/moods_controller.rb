4e1513e04dd7d19e767bbda8335f6f137a8f8716
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-09 12:52:58 +0200

    save mood in database

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,8 +1,21 @@
+require 'date'
+
 class MoodsController < ApplicationController
   def new
-    respond_to do |format|
-      format.html { redirect_to dashboard_path }
-      format.text { render partial: "pages/#{params[:m]}", formats: [:html] }
+    @chosenmood = params[:m]
+    @mood = Mood.new(mood: :"#{@chosenmood}")
+    @mood.user = current_user
+    @mood.date = Date.today
+    if @mood.save!
+      respond_to do |format|
+        format.html { redirect_to dashboard_path }
+        format.text { render partial: "pages/#{params[:m]}", formats: [:html] }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to dashboard_path }
+        format.text { render partial: "pages/mood", formats: [:html] }
+      end
     end
   end
 end
