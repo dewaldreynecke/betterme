4fe7df6208e18e3daf77db8853ae28f7e6d760ea
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-11 15:42:48 +0200

    implement destroy in controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,5 +3,8 @@ class AddressesController < ApplicationController
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to profile_path, status: :see_other
   end
 end
