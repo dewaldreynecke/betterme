2d910b4d71a1bf0232c1de5713d863f1ec9ef155
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-12 11:06:54 +0200

    add controller logic to change button classes

--- app/javascript/controllers/favourite_address_controller.js
+++ app/javascript/controllers/favourite_address_controller.js
@@ -9,10 +9,19 @@ export default class extends Controller {
 
   clickHandler(event) {
     event.preventDefault()
+    // save the address in the form field
     this.addressTarget.value = event.target.dataset.address
 
-    // nextElementSibling
-    this.btnTarget.classList.remove("btn-secondary")
-    this.btnTarget.classList.add("btn-primary")
+    // iterate through the buttons to activate the clicked one
+    // and deactivate the others
+    this.btnTargets.forEach(function (element) {
+      if (element.dataset.name === event.target.dataset.name) {
+        element.classList.remove("btn-secondary")
+        element.classList.add("btn-primary")
+      } else {
+        element.classList.remove("btn-primary")
+        element.classList.add("btn-secondary")
+      }
+    })
   }
 }
