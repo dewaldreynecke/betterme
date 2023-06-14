920e35587cbba03156533f2ce85f60c94ce57cb6
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-14 09:13:16 +0200

    remove entry_id column from tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_064600) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_071121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,10 +91,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_064600) do
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.bigint "user_id", null: false
-    t.bigint "entry_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["entry_id"], name: "index_tags_on_entry_id"
     t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
