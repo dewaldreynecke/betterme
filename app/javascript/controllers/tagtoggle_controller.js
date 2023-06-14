2d43bb779046a2e1158d251024bbe5b826c6c2ce
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-14 18:08:12 +0200

    add JS to style the tags

--- app/javascript/controllers/tagtoggle_controller.js
+++ app/javascript/controllers/tagtoggle_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tagtoggle"
 export default class extends Controller {
-  // static targets = ["tag"]
 
   connect() {
-    console.log("tagtoggle controller connected");
-  }
+    // select all the checkboxes
+    document.querySelectorAll(".form-check").forEach((form_check) => {
+      // give them their initial (unselected) style
+      form_check.lastChild.classList.add("button-17")
 
-  clickHandler(event) {
-    this.tagTarget.labels[0].classList.add("button-17")
+      // if clicked, toggle the style that makes them appear selected
+      form_check.addEventListener("click", (event) => {
+        if (event.target.localName === "label") {
+          event.originalTarget.classList.toggle("button-17")
+        }
+      });
+    });
   }
 }
