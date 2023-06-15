1474abda4ac26f03fc47ef87ad34bdd36a460450
Dewald Reynecke <dewaldreynecke@me.com>
2023-06-15 10:19:03 +0200

    style tag and address buttons

--- app/javascript/controllers/tagtoggle_controller.js
+++ app/javascript/controllers/tagtoggle_controller.js
@@ -4,15 +4,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
+    // turn off the label for the checboxes area
+    const checkLabel = document.querySelector("legend")
+    checkLabel.classList.add("d-none")
     // select all the checkboxes
     document.querySelectorAll(".form-check").forEach((form_check) => {
       // give them their initial (unselected) style
-      form_check.lastChild.classList.add("button-17")
+      form_check.lastChild.classList.add("tag-base")
 
       // if clicked, toggle the style that makes them appear selected
       form_check.addEventListener("click", (event) => {
         if (event.target.localName === "label") {
-          event.originalTarget.classList.toggle("button-17")
+          event.originalTarget.classList.toggle("tag-selected")
         }
       });
     });
